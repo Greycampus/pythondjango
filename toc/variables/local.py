9de19b19893abe3bcdc80694c6193e6d0977a6c6
r11n <raghavendra.n@greycampus.com>
2017-06-19 14:42:36 +0530

    added program map

--- toc/variables/local.py
+++ toc/variables/local.py
@@ -1,3 +1,6 @@
-k = input()
-while 1:
-    
+k = str(input('enter the number:'))
+k1 = int(k)
+ll = 2*len(k)
+lk = [abs(k1-3**i) for i in range(ll-2,ll+2)]
+ans = min(lk)
+print('the nearest power of three is:'+str(3**(ll-2+lk.index(ans))))
