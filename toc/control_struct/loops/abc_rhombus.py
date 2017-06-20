131627f72794ae7f881d960a4152779d906c8bdd
r11n <raghavendra.n@greycampus.com>
2017-06-20 16:39:29 +0530

    added program map to toc

--- toc/control_struct/loops/abc_rhombus.py
+++ toc/control_struct/loops/abc_rhombus.py
@@ -1,8 +1,8 @@
 k = int(input('enter number:'))
 w = int(input('enter width:'))
 for i in range(0,k):
-    #using center as padding of string functions
-    #using ascii codes for printing the getting the alphabet based on loop
+    #using center function for padding from string functions
+    #using ascii codes for printing the getting the alphabet based on loop va
     #increament loop
     print(str(chr(65+i)*(2*i+1)).center(w,'-'))
 for i in range(1,k):
